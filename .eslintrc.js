ae83734d202a816ee3ddda08121d77eeea923361
Guilherme Felipe Radunz do Ros�rio <guilherme@cargon.com.br>
2024-01-11 00:57:42 -0300

    chore: add typescript, eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parser: "@babel/eslint-parser",
+    root: true,
+    plugins: ['@typescript-eslint', 'import', 'prettier'],
+    extends: [
+      'airbnb-typescript/base',
+      'prettier',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.eslint.json',
+    },
+    
+  };
