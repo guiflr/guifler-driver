739e22878fdf97701fd82457bab02eb4532246c2
guilherme <guilherme@cargon.com.br>
2024-01-14 20:57:04 -0300

    feat: add errorMiddleware

---
+++ src/shared/http/middlewares/errorMiddleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err.status) {
+    return res.status(err.status).json(err);
+  }
+
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
