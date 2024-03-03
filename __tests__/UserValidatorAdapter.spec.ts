48c1898c3d8c8946d209387dfe4b0cd7ab929d7a
guilherme <guiflr97@gmail.com>
2024-03-03 11:23:20 -0300

    test: ensure user create validator is validating all fields and password confirmation

--- __tests__/UserValidatorAdapter.spec.ts
+++ __tests__/UserValidatorAdapter.spec.ts
@@ -5,14 +5,32 @@ import { addUserData } from './factory/fakeData';
 describe('UserValidatorAdapter', () => {
   const validator = new UserValidatorAdapter();
   test('Should return error', () => {
-    const data: UserCreateModel = null as unknown as UserCreateModel;
+    const data: UserCreateModel = {} as unknown as UserCreateModel;
 
     const response = validator.validator(data);
 
     expect(response.isValid).toBe(false);
-    expect(response.error).toEqual(
-      '{"issues":[{"code":"invalid_type","expected":"object","received":"null","path":[],"message":"Expected object, received null"}],"name":"ZodError"}'
-    );
+    expect(response.error).toEqual([
+      'email is required',
+      'username is required',
+      'role is required',
+      'password is required',
+      'passwordConfirm is required',
+    ]);
+  });
+
+  test('Should return password confirm error', () => {
+    const data: UserCreateModel = {
+      email: 'guilherme@gmail.com',
+      password: 'abc123',
+      passwordConfirm: '123abc',
+      role: 'admin',
+      username: 'gui',
+    };
+
+    const response = validator.validator(data);
+
+    expect(response.error).toEqual(['password and password confirmation is not equal']);
   });
 
   test('Should return valid schema', () => {
