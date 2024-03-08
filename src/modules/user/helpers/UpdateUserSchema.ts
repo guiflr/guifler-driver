12f7bab440d12db77cbbc2f941bb522e87c823d0
guilherme <guiflr97@gmail.com>
2024-03-08 16:46:53 -0300

    refactor: remove email validator on update user

--- src/modules/user/helpers/UpdateUserSchema.ts
+++ src/modules/user/helpers/UpdateUserSchema.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 export const UpdateUserSchema = z.object({
-  email: z.string().email({ message: 'email is invalid' }),
   username: z.string({ required_error: 'username is required' }),
   role: z.enum(['admin', 'creator', 'guest'], {
     required_error: 'role is required',
